bc46bb3101b22d129e732a88d8f989c5b6e2f496
Patrik Luiz <111705938+zPlcs@users.noreply.github.com>
2025-03-10 09:14:20 -0300

    Create Style.js
    Added base only style.

---
+++ Style.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from 'react-native'
+
+const Style = StyleSheet.create ({
+  main: {
+    flex: 1,
+    alignItems: 'center'
+  },
+
+  input:{
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+  },
+  
+  lowOpacity:{
+    opacity: 0.5
+  },
+
+  normalOpacity:{
+    opacity: 1
+  },
+})
+
+export default Style
